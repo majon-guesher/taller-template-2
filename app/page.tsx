0c270623bae6fd21c9857aac715a7b18112fc7e4
matekno <mati.rapoport@gmail.com>
2026-05-05 20:04:41 -0300

    Suma juego espia lunar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black px-6 text-center text-stone-100">
-      <h1 className="text-5xl font-semibold tracking-tight sm:text-7xl">
-        Hola! Soy el taller 2
-      </h1>
-    </main>
-  );
+  redirect("/luna-espia/index.html");
 }
